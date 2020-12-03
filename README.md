0a5e8d43f8d3c634204e1625abf9af0117b06263
Federico Ferri <federico.ferri.it@gmail.com>
2020-12-03 16:35:35 +0100

    make xmlschema optional

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Minimum versions:
 
 - C++17 compiler (Visual C++ 2019, gcc-7, clang-3.9)
 - CMake 3.16.3
-- Python 3.6 with the following pip packages: `xmlschema`
-- xsltproc (windows binary available [here](https://github.com/CoppeliaRobotics/xsltproc-win/raw/master/xsltproc-win.zip))
+- xsltproc (available via `apt`/`brew`/`choco`)
+- Python 3.6
+- (optional) Python package 'xmlschema' (`pip install xmlschema`)
 
 ## See also
 
--- simStubsGen/parse.py
+++ simStubsGen/parse.py
@@ -2,20 +2,15 @@ import os
 import sys
 import xml.etree.ElementTree as ET
 
-try:
-    import xmlschema
-except ModuleNotFoundError:
-    print('\n\n{}\n\n'.format('\n'.join('   ' + x for x in [
-        'error: Python package "xmlschema" not found.',
-        '       Please install it, e.g.: python3 -m pip install xmlschema'
-    ])))
-    sys.exit(1)
-
 import model
 
 def parse(xml_file):
-    schema = xmlschema.XMLSchema11(os.path.dirname(os.path.realpath(__file__)) + '/xsd/callbacks.xsd')
-    schema.validate(xml_file)
+    try:
+        import xmlschema
+        schema = xmlschema.XMLSchema11(os.path.dirname(os.path.realpath(__file__)) + '/xsd/callbacks.xsd')
+        schema.validate(xml_file)
+    except ModuleNotFoundError:
+        print(f'warning: missing python package "xmlschema"; input file {os.path.basename(xml_file)} will not be validated.', file=sys.stderr)
     tree = ET.parse(xml_file)
     root = tree.getroot()
     return model.Plugin(root)
