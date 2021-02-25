d07d7a3895e3c88fa088616a7aec592a8697b619
Federico Ferri <federico.ferri.it@gmail.com>
2021-02-25 10:07:45 +0100

    new methods: add, remove, find

--- simPlusPlus/Handle.h
+++ simPlusPlus/Handle.h
@@ -6,55 +6,104 @@
 #include <boost/regex.hpp>
 #include <boost/lexical_cast.hpp>
 
+#include <simPlusPlus/Lib.h>
+
 namespace sim
 {
-	/*! \brief A tool for converting pointers to strings and vice versa.
+    /*! \brief A tool for converting pointers to strings and vice versa.
      *
      * Usage: specialize the Handle<T>::tag() method for your class, e.g.:
      *
      * template<> std::string Handle<OcTree>::tag() { return "octomap.OcTree"; }
      *
      * Note: tag must not contain ":"
-	 */
+     */
     template<typename T>
     struct Handle
     {
-		static std::string str(const T *t)
-		{
-			static boost::format fmt("%s:%lld:%d");
-			return (fmt % tag() % reinterpret_cast<long long int>(t) % crc_ptr(t)).str();
-		}
+        static std::string str(const T *t)
+        {
+            static boost::format fmt("%s:%lld:%d");
+            return (fmt % tag() % reinterpret_cast<long long int>(t) % crc_ptr(t)).str();
+        }
 
-		static T * obj(std::string h)
-		{
-			boost::cmatch m;
-			static boost::regex re("([^:]+):([^:]+):([^:]+)");
-			if(boost::regex_match(h.c_str(), m, re) && m[1] == tag())
-			{
-				T *t = reinterpret_cast<T*>(boost::lexical_cast<long long int>(m[2]));
-				int crc = boost::lexical_cast<int>(m[3]);
-				if(crc == crc_ptr(t)) return t;
-			}
-			return nullptr;
-		}
+        static T * obj(std::string h)
+        {
+            boost::cmatch m;
+            static boost::regex re("([^:]+):([^:]+):([^:]+)");
+            if(boost::regex_match(h.c_str(), m, re) && m[1] == tag())
+            {
+                T *t = reinterpret_cast<T*>(boost::lexical_cast<long long int>(m[2]));
+                int crc = boost::lexical_cast<int>(m[3]);
+                if(crc == crc_ptr(t)) return t;
+            }
+            return nullptr;
+        }
 
-	private:
-		static std::string tag()
-		{
-			return "ptr";
-		}
+        static std::string add(const T *t, int scriptID)
+        {
+            int sceneID = getSceneID(scriptID);
+            handles[sceneID][scriptID].insert(t);
+            handlesr[t][sceneID] = scriptID;
+            return str(t);
+        }
 
-		static int crc_ptr(const T *t)
-		{
-			auto x = reinterpret_cast<long long int>(t);
-			x = x ^ (x >> 32);
-			x = x ^ (x >> 16);
-			x = x ^ (x >> 8);
-			x = x ^ (x >> 4);
-			x = x & 0x000000000000000F;
-			x = x ^ 0x0000000000000008;
-			return int(x);
-		}
+        static void remove(const T *t)
+        {
+            for(const auto &m : handlesr.at(t))
+            {
+                int sceneID = m.first;
+                int scriptID = m.second;
+                handles.at(sceneID).at(scriptID).remove(t);
+            }
+            handlesr.erase(t);
+        }
+
+        static std::set<T*> find(int scriptID)
+        {
+            int sceneID = getSceneID(scriptID);
+            return handles.at(sceneID).at(scriptID);
+        }
+
+    private:
+        static int getSceneID(int scriptID)
+        {
+            int scriptType, objectHandle;
+            sim::getScriptProperty(scriptID, &scriptType, &objectHandle);
+            if(0
+                    || scriptType == sim_scripttype_mainscript
+                    || scriptType == sim_scripttype_childscript
+                    || scriptType == sim_scripttype_customizationscript
+            )
+                return sim::getInt32Parameter(sim_intparam_scene_unique_id);
+            else
+                return -1;
+        }
+
+        static std::string tag()
+        {
+            return "ptr";
+        }
+
+        static int crc_ptr(const T *t)
+        {
+            auto x = reinterpret_cast<long long int>(t);
+            x = x ^ (x >> 32);
+            x = x ^ (x >> 16);
+            x = x ^ (x >> 8);
+            x = x ^ (x >> 4);
+            x = x & 0x000000000000000F;
+            x = x ^ 0x0000000000000008;
+            return int(x);
+        }
+
+        // Tables of created objects (for methods: add, remove, find)
+
+        // sceneID -> (scriptID -> [objects])
+        static std::map<int, std::map<int, std::set<T*>>> handles;
+
+        // object -> (sceneID -> scriptID)
+        static std::map<T*, std::map<int, int>> handlesr;
     };
 }
 
